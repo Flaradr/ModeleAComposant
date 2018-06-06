6da0ab6245205e8723e9c3c625fe4c1ce9e720c1
Flaradr <matthieugayraud@gmail.com>
2018-06-06 10:34:56 +0200

    Modify generator, can generate file for component and assembly

--- com.project.foo/src/com/project/foo/generator/FooGenerator.xtend
+++ com.project.foo/src/com/project/foo/generator/FooGenerator.xtend
@@ -7,6 +7,13 @@ import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.xtext.generator.AbstractGenerator
 import org.eclipse.xtext.generator.IFileSystemAccess2
 import org.eclipse.xtext.generator.IGeneratorContext
+import com.project.foo.foo.Model
+import com.project.foo.foo.Component
+import com.project.foo.foo.Assembly
+import com.project.foo.foo.ProvidedService
+import com.project.foo.foo.RequiredService
+import com.project.foo.foo.Parameter
+import com.project.foo.foo.PSignature
 
 /**
  * Generates code from your model files on save.
@@ -16,10 +23,53 @@ import org.eclipse.xtext.generator.IGeneratorContext
 class FooGenerator extends AbstractGenerator {
 
 	override void doGenerate(Resource resource, IFileSystemAccess2 fsa, IGeneratorContext context) {
-//		fsa.generateFile('greetings.txt', 'People to greet: ' + 
-//			resource.allContents
-//				.filter(Greeting)
-//				.map[name]
-//				.join(', '))
+		for(c : resource.allContents.toIterable.filter(Component)){
+			fsa.generateFile(c.name+".java", c.compile())
+		}
+		
+		for(a : resource.allContents.toIterable.filter(Assembly)){
+			fsa.generateFile(a.name+".java", a.compile())
+		}
 	}
+	
+	def compile(Component c) '''
+    package «(c.eContainer as Model).name»;
+        
+    public class «c.name» {
+    	«FOR ProvidedService servicesP : c.listOfPServices.providedServices»
+    		«servicesP.compile()»
+    	«ENDFOR»
+    	
+    	«FOR RequiredService servicesR : c.listOfRServices.requiredServices»
+    		«servicesR.compile()»
+    	«ENDFOR»
+    }
+	'''
+
+	def compile(ProvidedService sp)'''
+	private «sp.name.type» «sp.name.name»(«loopParameter(sp)»{
+	}
+	'''
+	
+	def compile(Parameter p)'''
+	«p.type» «p.name»,
+	'''
+	
+	def compile(RequiredService sr)'''
+	private «sr.name.type» «sr.name.name»{
+	}
+	'''
+	
+	def compile(Assembly a) '''
+    package «(a.eContainer as Model).name»;
+        
+    public class «a.name» {
+    	
+    }
+	'''
+	
+	//Donne la signature du service fourni sp
+	def loopParameter(ProvidedService sp)'''
+	«FOR Parameter p : (sp.eCrossReferences.get(0) as PSignature).parameters»«p.compile()»«ENDFOR»
+	'''
 }
--- com.project.foo/xtend-gen/com/project/foo/generator/FooGenerator.java
+++ com.project.foo/xtend-gen/com/project/foo/generator/FooGenerator.java
@@ -3,10 +3,22 @@
  */
 package com.project.foo.generator;
 
+import com.google.common.collect.Iterables;
+import com.project.foo.foo.Assembly;
+import com.project.foo.foo.Component;
+import com.project.foo.foo.Model;
+import com.project.foo.foo.PSignature;
+import com.project.foo.foo.Parameter;
+import com.project.foo.foo.ProvidedService;
+import com.project.foo.foo.RequiredService;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.generator.AbstractGenerator;
 import org.eclipse.xtext.generator.IFileSystemAccess2;
 import org.eclipse.xtext.generator.IGeneratorContext;
+import org.eclipse.xtext.xbase.lib.IteratorExtensions;
 
 /**
  * Generates code from your model files on save.
@@ -17,5 +29,138 @@ import org.eclipse.xtext.generator.IGeneratorContext;
 public class FooGenerator extends AbstractGenerator {
   @Override
   public void doGenerate(final Resource resource, final IFileSystemAccess2 fsa, final IGeneratorContext context) {
+    Iterable<Component> _filter = Iterables.<Component>filter(IteratorExtensions.<EObject>toIterable(resource.getAllContents()), Component.class);
+    for (final Component c : _filter) {
+      String _name = c.getName();
+      String _plus = (_name + ".java");
+      fsa.generateFile(_plus, this.compile(c));
+    }
+    Iterable<Assembly> _filter_1 = Iterables.<Assembly>filter(IteratorExtensions.<EObject>toIterable(resource.getAllContents()), Assembly.class);
+    for (final Assembly a : _filter_1) {
+      String _name_1 = a.getName();
+      String _plus_1 = (_name_1 + ".java");
+      fsa.generateFile(_plus_1, this.compile(a));
+    }
+  }
+  
+  public CharSequence compile(final Component c) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("package ");
+    EObject _eContainer = c.eContainer();
+    String _name = ((Model) _eContainer).getName();
+    _builder.append(_name);
+    _builder.append(";");
+    _builder.newLineIfNotEmpty();
+    _builder.append("    ");
+    _builder.newLine();
+    _builder.append("public class ");
+    String _name_1 = c.getName();
+    _builder.append(_name_1);
+    _builder.append(" {");
+    _builder.newLineIfNotEmpty();
+    {
+      EList<ProvidedService> _providedServices = c.getListOfPServices().getProvidedServices();
+      for(final ProvidedService servicesP : _providedServices) {
+        _builder.append("\t");
+        CharSequence _compile = this.compile(servicesP);
+        _builder.append(_compile, "\t");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append("\t");
+    _builder.newLine();
+    {
+      EList<RequiredService> _requiredServices = c.getListOfRServices().getRequiredServices();
+      for(final RequiredService servicesR : _requiredServices) {
+        _builder.append("\t");
+        CharSequence _compile_1 = this.compile(servicesR);
+        _builder.append(_compile_1, "\t");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append("}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence compile(final ProvidedService sp) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("private ");
+    String _type = sp.getName().getType();
+    _builder.append(_type);
+    _builder.append(" ");
+    String _name = sp.getName().getName();
+    _builder.append(_name);
+    _builder.append("(");
+    CharSequence _loopParameter = this.loopParameter(sp);
+    _builder.append(_loopParameter);
+    _builder.append("{");
+    _builder.newLineIfNotEmpty();
+    _builder.append("}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence compile(final Parameter p) {
+    StringConcatenation _builder = new StringConcatenation();
+    String _type = p.getType();
+    _builder.append(_type);
+    _builder.append(" ");
+    String _name = p.getName();
+    _builder.append(_name);
+    _builder.append(",");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+  
+  public CharSequence compile(final RequiredService sr) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("private ");
+    String _type = sr.getName().getType();
+    _builder.append(_type);
+    _builder.append(" ");
+    String _name = sr.getName().getName();
+    _builder.append(_name);
+    _builder.append("{");
+    _builder.newLineIfNotEmpty();
+    _builder.append("}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence compile(final Assembly a) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("package ");
+    EObject _eContainer = a.eContainer();
+    String _name = ((Model) _eContainer).getName();
+    _builder.append(_name);
+    _builder.append(";");
+    _builder.newLineIfNotEmpty();
+    _builder.append("    ");
+    _builder.newLine();
+    _builder.append("public class ");
+    String _name_1 = a.getName();
+    _builder.append(_name_1);
+    _builder.append(" {");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence loopParameter(final ProvidedService sp) {
+    StringConcatenation _builder = new StringConcatenation();
+    {
+      EObject _get = sp.eCrossReferences().get(0);
+      EList<Parameter> _parameters = ((PSignature) _get).getParameters();
+      for(final Parameter p : _parameters) {
+        CharSequence _compile = this.compile(p);
+        _builder.append(_compile);
+      }
+    }
+    _builder.newLineIfNotEmpty();
+    return _builder;
   }
 }
